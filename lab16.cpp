6ea4bb515232f4859d99666bf7099745cd90813e
Makenzie Burwell <makbur@umich.edu>
2025-03-31 12:17:10 -0400

    3rd

--- lab16.cpp
+++ lab16.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
+// Function to calculate the Greatest Common Factor (GCF) using recursion
 int gcf(int a, int b) {
     if (b == 0) {
-        return a;
+        return a;  // Base case: If b is 0, return a
+    } else {
+        return gcf(b, a % b);  // Recursive case: gcf(b, a % b)
     }
-    return gcf(b, a % b);
 }
 
 int main() {
-    int a, b;
-    cout << "Enter two integers to find the GCF: ";
-    cin >> a >> b;
-    cout << "GCF: " << gcf(a, b) << endl;
+    // Test case: Find the GCF of 888 and 54
+    cout << gcf(888, 54) << endl;  // Expected output: 6
     return 0;
 }
