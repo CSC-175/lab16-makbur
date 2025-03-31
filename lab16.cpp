fcc0e612f9cf59db3daa8d58f439ed9266d84ab7
Makenzie Burwell <makbur@umich.edu>
2025-03-31 12:24:07 -0400

    5th

--- lab16.cpp
+++ lab16.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 using namespace std;
 
-// Function to calculate the Greatest Common Factor (GCF) using recursion
+// Recursive function to find GCF (Greatest Common Factor) using Euclidean algorithm
 int gcf(int a, int b) {
     if (b == 0) {
-        return a;  // Base case: If b is 0, return a
-    } else {
-        return gcf(b, a % b);  // Recursive case: gcf(b, a % b)
+        return a; // Base case: when b is zero, return a
     }
+    return gcf(b, a % b); // Recursively call with b and a % b
 }
 
 int main() {
+    // Input two integer values
     int a, b;
-
-    // Prompt user for input
     cout << "Enter two integer values you want to find the GCF of: ";
     cin >> a >> b;
 
-    // Output the result of the GCF calculation
-    cout << "The greatest common factor of " << a << " and " << b << " is " << gcf(a, b) << endl;
-
+    // Output the result
+    cout << "The greatest common factor of " << a  <<" and " << b << " is " << gcf(a,b) << endl;
     return 0;
 }
