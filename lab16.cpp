edfbdea4c399aa151d00b2bcced8a8bc25b0cad1
Makenzie Burwell <makbur@umich.edu>
2025-03-31 12:10:48 -0400

    2nd

--- lab16.cpp
+++ lab16.cpp
@@ -1,29 +1,17 @@
-// Code to implement the gcf function goes here
 #include <iostream>
 using namespace std;
 
-// Function prototype
-int gcf(int a, int b);
-
-// Function to calculate the GCF using Euclidean Algorithm
 int gcf(int a, int b) {
-    // Base case: if b is 0, then the GCF is a
     if (b == 0) {
         return a;
     }
-    // Recursive call: calculate GCF of b and a % b
     return gcf(b, a % b);
 }
 
 int main() {
     int a, b;
-
-    // Prompt user for input
-    cout << "Enter two integer values you want to find the GCF of: ";
+    cout << "Enter two integers to find the GCF: ";
     cin >> a >> b;
-
-    // Calculate and print the GCF
-    cout << "The greatest common factor of " << a << " and " << b << " is " << gcf(a, b) << endl;
-
+    cout << "GCF: " << gcf(a, b) << endl;
     return 0;
 }
