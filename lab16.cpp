5a7ee4d4637ec01f24d91b51fd6d62aed95aaeb8
Makenzie Burwell <makbur@umich.edu>
2025-03-31 12:50:32 -0400

    10th

--- lab16.cpp
+++ lab16.cpp
@@ -1,22 +1,4 @@
-#include <iostream>
-using namespace std;
-
-// Function prototype for the gcf function
-int gcf(int a, int b);
-
-int main()
-{
-    int a, b;
-
-    // Ask the user for two integer values
-    cout << "Enter two integer values you want to find the GCF of: ";
-    cin >> a >> b;
-
-    // Output the greatest common factor
-    cout << "The greatest common factor of " << a << " and " << b << " is " << gcf(a, b) << endl;
-
-    return 0;
-}
+// lab16.cpp
 
 // Recursive function to calculate the GCF using the Euclidean algorithm
 int gcf(int a, int b)
